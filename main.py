7e66b8bd1911ced0642fbacd226a252a12a749bc
Dynamo13 <62235947+Dynamo13@users.noreply.github.com>
2023-08-17 12:30:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ from model import*
 from dataloader import*
 import os
 import random
+import sys
 
-def main(input_dir, mask_dir, image_height, image_width, image_channel, num_classes, batch_size, epochs, val_samples):
+def main(input_dir, mask_dir,weight_dir,
+    image_height=128,
+    image_width=128,
+    image_channel=3,
+    img_size = (128,128),
+    num_classes = 4,
+    batch_size = 8,
+    epochs=200,
+    val_samples = 250,):
 
     img_size = (image_height,image_width)
     input_img_paths = sorted(
@@ -34,20 +43,20 @@ def main(input_dir, mask_dir, image_height, image_width, image_channel, num_clas
     model=AW_Net((image_height,image_width,image_channel),num_classes, dropout_rate=0.0, batch_norm=True)
 
     model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
-    
-    history = model.fit(train_gen,validation_data=val_gen,epochs=epochs,)
+    checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+    filepath=weight_dir+"\\weights.h5",
+    save_weights_only=True,
+    monitor='val_loss',
+    mode='min',
+    save_best_only=True
+    )
+    es=tf.keras.callbacks.EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=8)
+    history = model.fit(train_gen,validation_data=val_gen,epochs=epochs,callbacks=[checkpoint_callback,es])
 
 
 if __name__ == "__main__":
-    input_dir = "E:\\BraTS_data\\Image"
-    mask_dir = "E:\\BraTS_data\\Mask"
-    image_height=128
-    image_width=128
-    image_channel=3
-    img_size = (image_height,image_width)
-    num_classes = 4
-    batch_size = 8
-    epochs=200
-    val_samples = 250
-    main(input_dir, mask_dir, image_height, image_width, image_channel, num_classes, batch_size, epochs, val_samples)
+    img_dir = sys.argv[1]
+    mask_dir = sys.argv[2]
+    weight_dir = sys.argv[3]
+    main(img_dir,mask_dir,weight_dir)
 	
